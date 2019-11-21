911dab225193904d66b2a25c0afb979dd5568801
sfarn <sfarn48@gmail.com>
2019-11-21 16:18:41 -0500

    Timer script

---
+++ Sonic R Final/Assets/Script/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public int totalTime;
+    public int min;
+    public int sec;
+    public int secFraction;
+    public Text finalTime;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime += Mathf.RoundToInt(Time.deltaTime * 100);
+        secFraction = totalTime %100;
+        min = totalTime / 6000;
+        sec = totalTime / 100-(min*60);
+        finalTime.text = (min+":"+sec+":"+secFraction);
+    }
+    
+}
