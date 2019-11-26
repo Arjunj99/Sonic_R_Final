4ef4da3a35acf9a614760f30d6e238b429fe9c7f
hankf007 <hankf007@gmail.com>
2019-11-25 20:55:26 -0500

    position in race

--- Sonic R Final/Assets/Script/Laps.cs
+++ Sonic R Final/Assets/Script/Laps.cs
@@ -10,10 +10,17 @@ public class Laps : MonoBehaviour
     public GameObject mark3;
 
     public int lapNum;
+    public float markNum;
+
+    public int mark;
 
     public bool passed1 = false;
     public bool passed2 = false;
     public bool passed3 = false;
+
+    public float distanceToNext;
+
+    
 
 
     // Start is called before the first frame update
@@ -34,6 +41,10 @@ public class Laps : MonoBehaviour
         {
             passed1 = true;
 
+            mark = 1;
+
+            distanceToNext= Vector3.Distance(this.transform.position, mark2.transform.position);
+
             if (passed3 == true)
             {
                 lapNum++;
@@ -44,6 +55,8 @@ public class Laps : MonoBehaviour
 
         if (collision.gameObject.name == "Cube2")
         {
+            distanceToNext = Vector3.Distance(this.transform.position, mark3.transform.position);
+            mark = 2;
             if (passed1 == true)
             {
                 passed2 = true;
@@ -54,6 +67,8 @@ public class Laps : MonoBehaviour
 
         if (collision.gameObject.name == "Cube3")
         {
+            distanceToNext = Vector3.Distance(this.transform.position, mark1.transform.position);
+            mark = 3;
             if (passed2 == true)
             {
                 passed3 = true;
---
+++ Sonic R Final/Assets/Script/Position.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+
+    public GameObject player1;
+    public GameObject player2;
+
+    public bool player1Infront;
+
+   //public Laps lapscript1;
+    public int lapNum1;
+    public int lapNum2;
+    public float distance1;
+    public float distance2;
+    public float mark1;
+    public float mark2;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lapNum1 = GameObject.Find("Player 1").GetComponent<Laps>().lapNum;
+        lapNum2 = GameObject.Find("Player 2").GetComponent<Laps>().lapNum;
+        distance1 = GameObject.Find("Player 1").GetComponent<Laps>().distanceToNext;
+        distance2 = GameObject.Find("Player 2").GetComponent<Laps>().distanceToNext;
+        mark1 = GameObject.Find("Player 1").GetComponent<Laps>().mark;
+        mark2 = GameObject.Find("Player 2").GetComponent<Laps>().mark;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lapNum1 > lapNum2)
+        {
+            player1Infront = true;
+
+        }
+        else if (lapNum1 < lapNum2)
+        {
+            player1Infront = false;
+        }
+        else if (lapNum1 == lapNum2)
+        {
+            if (mark1 > mark2)
+            {
+                player1Infront = true;
+            }
+            else if (mark1 < mark2)
+            {
+                player1Infront = false;
+            }
+            if (mark1 == mark2)
+            {
+                if (distance1 < distance2)
+                {
+                    player1Infront = true;
+                }
+                else if (distance1 > distance2)
+                {
+                    player1Infront = false;
+                }
+
+            }
+        }
+
+
+
+        if (player1Infront == true)
+        {
+
+        }
+        else if (player1Infront == false)
+        {
+
+        }
+    }
+}
