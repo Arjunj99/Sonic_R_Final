e6c8dce5fc7cf9cae54168a095e9d20f8d8089ab
Arjunj99 <asj397@nyu.edu>
2019-11-29 23:23:08 -0500

    Fine Tuning the Movement Script

--- Sonic R Final/Assets/Script/Arjun's Revamp Scripts/CharacterMovement.cs
+++ Sonic R Final/Assets/Script/Arjun's Revamp Scripts/CharacterMovement.cs
@@ -3,32 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // [Header("Max Speeds (Surfaces)")]
-    // [SerializeField] public List<float> surfaceSpeeds = new List<float>(); // List of Speeds based on Surface Type
-    // [HideInInspector] public enum trackType {track, sand, grass, water}; // List of Surface Types
-    // public trackType currentSurfaceType; // Current Surface Type
-    // [HideInInspector] public float currentVelocity;
-    // [HideInInspector] public float maxVelocity;
-    // [HideInInspector] public float currentTime; // How long the player has been holding down the forward Key
-    // public float timePeriod = 2; // How long it takes for player to reach max speed
-
-    // [Header("Velocity Speeds")]
-    // public float velocitySmooth; // The value used to lerp between speeds
-    // public AnimationCurve velocityCurve; // Animation Curve used to shape acceleration
-    // private Rigidbody rb;
-    // private CharacterController cc;
-    // private bool inAir; // If the GameObject is in the Air
-
-    // [Header("Button Settings")]
-    // [SerializeField] private KeyCode forwardKey; // Key for going forward
-    // [SerializeField] private KeyCode brakeKey; // Key for braking
-
-    // Vector3 moveDirection;
-    // public float jumpSpeed;
-    // public float gravity;
-
+public class CharacterMovement : MonoBehaviour {   
     private CharacterController characterController;
 
     [Header("Button Settings")]
@@ -51,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
     private float angularT = 0.5f;
     public float angularTimePeriod;
     public Vector2 inputAxis;
+    private Vector3 rotation = Vector3.zero;
 
 
 
@@ -81,29 +57,47 @@ public class PlayerMovement : MonoBehaviour
     /// </summary>
     private void GroundedMovement() {
         if (this.characterController.isGrounded) { // What player can do when grounded
-            jumpsLeft = 2; // Resets Double Jump (WONT WORK WITH MOUNTAIN)
+            // jumpsLeft = 2; // Resets Double Jump (WONT WORK WITH MOUNTAIN)
             Movement(); // Allows for movement while grounded
             gravity = 9.81f;
-            Jump();
+
+
+            if (Input.GetButton(jumpButton)) {
+                moveDirection.y = jumpHeight;
+            }
             
         } else { // What player can do in the air
-            gravity += gravity * Time.deltaTime;
+            // gravity += gravity * Time.deltaTime;
         }
 
-        
+        rotation = new Vector3(0f, Input.GetAxis(horizontalAxis), 0f);
+        rotation *= turnSpeed;
+        Debug.Log("Horizontal:" + rotation);
 
-        gameObject.transform.Rotate(new Vector3(0f, angularVelocity.Evaluate(angularT) * turnSpeed, 0f));
+        // Apply gravity
+        moveDirection.y -= gravity * Time.deltaTime;
 
         moveDirection = gameObject.transform.forward * forwardVelocity.Evaluate(forwardT) * maxSpeed;
+
+        moveDirection.y -= gravity;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+        characterController.transform.Rotate(rotation);
+
         
-        moveDirection.y -= gravity; // Adds effect of gravity to the player
+
+        // gameObject.transform.Rotate(new Vector3(0f, angularVelocity.Evaluate(angularT) * turnSpeed, 0f));
+
+        
+         // Adds effect of gravity to the player
 
         // Debug.Log("MOVE DIRECTION: " + moveDirection);
         
-        Turn();
+        // Turn();
         
-        characterController.Move(moveDirection * Time.deltaTime);
-        Debug.Log(moveDirection);
+        // characterController.Move(moveDirection * Time.deltaTime);
+        // Debug.Log(moveDirection);
     }
 
 
@@ -121,27 +115,5 @@ public class PlayerMovement : MonoBehaviour
             } else {
                 forwardT = 0;
             }
-    }
-
-    private void Turn() {
-        if (inputAxis.x > deadZone && angularT < 1f) {
-            angularT += (Time.deltaTime / angularTimePeriod);
-        } else if (inputAxis.x > deadZone && angularT > 1f) {
-            angularT = 1f;
-        } else if (inputAxis.x < -deadZone && angularT > 0) {
-            angularT -= (Time.deltaTime * 5 / angularTimePeriod);
-        } else if (inputAxis.x < -deadZone && angularT < 0) {
-            angularT = 0;
-        } else if (angularT > 0.51f || angularT < 0.49f) {
-            angularT = Mathf.Lerp(angularT, 0.5f, turnSmooth * Time.deltaTime);
-        } else {
-            angularT = 0.5f;
-        }
-    }
-
-    private void Jump() {
-        if (Input.GetButton(jumpButton)) {
-                moveDirection.y = jumpHeight;
-        }
     }
 }
