79237eb2a02568e44cc1c1b4471d57ccb1d1bc83
hankf007 <hankf007@gmail.com>
2019-12-04 22:37:04 -0500

    .

--- Sonic R Final/Assets/Script/items.cs
+++ Sonic R Final/Assets/Script/items.cs
@@ -51,7 +51,7 @@ public class items : MonoBehaviour
             }
             if(hasConfuse == true)
             {
-                
+                otherPlayer.GetComponent<CharacterMovement>().invert = true;
             }
         }
 
