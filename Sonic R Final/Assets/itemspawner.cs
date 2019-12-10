a408e5b3d0ff3c15ab305df345d1bbbaeaf177af
hankf007 <hankf007@gmail.com>
2019-12-10 15:40:22 -0500

    item spawn update and item rotation

--- Sonic R Final/Assets/itemspawner.cs
+++ Sonic R Final/Assets/itemspawner.cs
@@ -7,22 +7,39 @@ public class itemspawner : MonoBehaviour
     public GameObject squid;
     public GameObject confuse;
     public GameObject waterRun;
+    public GameObject waterRun1;
     public GameObject ring;
-
+    public GameObject ringx5;
+    public GameObject ringx10;
+    public GameObject ringx20;
+    /////////////////////////////////////////////////
     public Vector3 squidPos;
     public Vector3 confusePos;
     public Vector3 waterPos;
+    public Vector3 waterPos1;
     public Vector3 ringPos;
-
+    public Vector3 ringx5Pos;
+    public Vector3 ringx10Pos;
+    public Vector3 ringx20Pos;
+    /////////////////////////////////////////////////
     public float squidTimer;
     public float confuseTimer;
     public float waterTimer;
+    public float waterTimer1;
     public float ringTimer;
-
+    public float ringx5Timer;
+    public float ringx10Timer;
+    public float ringx20Timer;
+    /////////////////////////////////////////////////
     public bool squidGone;
     public bool confuseGone;
     public bool waterGone;
+    public bool waterGone1;
     public bool ringGone;
+    public bool ringx5Gone;
+    public bool ringx10Gone;
+    public bool ringx20Gone;
+
 
     public float spawnTime;
     // Start is called before the first frame update
@@ -30,8 +47,11 @@ public class itemspawner : MonoBehaviour
     {
         squidPos = squid.transform.position;
         confusePos = confuse.transform.position;
+        waterPos = waterRun.transform.position;
+        waterPos1 = waterRun1.transform.position;
+       
     }
-
+        
     // Update is called once per frame
     void Update()
     {
@@ -56,5 +76,26 @@ public class itemspawner : MonoBehaviour
                 confuseTimer = 0;
             }
         }
+        if (waterGone == true)
+        {
+            waterTimer += Time.deltaTime;
+            if (waterTimer > spawnTime)
+            {
+                Instantiate(waterRun, waterPos, Quaternion.identity);
+                waterGone = false;
+                waterTimer = 0;
+            }
+        }
+        if (waterGone1 == true)
+        {
+            waterTimer1 += Time.deltaTime;
+            if (waterTimer1 > spawnTime)
+            {
+                Instantiate(waterRun1, waterPos1, Quaternion.identity);
+                waterGone1 = false;
+                waterTimer1 = 0;
+            }
+        }
+       
     }
 }
