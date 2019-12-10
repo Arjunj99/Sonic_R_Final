63c864e92ab12a620aec446d7c69155598dcac5f
sfarn <sfarn48@gmail.com>
2019-12-10 15:44:04 -0500

    edited pause screen script, put into stephdevscene

--- Sonic R Final/Assets/Script/Arjun's Revamp Scripts/GameManager.cs
+++ Sonic R Final/Assets/Script/Arjun's Revamp Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public static GameManager instance;
-    public float gravity;
+    public float deadZone;
 
     // Start is called before the first frame update
     void Awake() {
