2a823822ab1150c599206e76dfd0c417e4e2e222
sfarn <sfarn48@gmail.com>
2019-12-10 22:33:31 -0500

    Created pause screen script and UI

--- Sonic R Final/Assets/Script/PauseScreen.cs
+++ Sonic R Final/Assets/Script/PauseScreen.cs
@@ -1,29 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PauseScreen : MonoBehaviour
 {
-    public GameObject pauseScreen;
-    public GameObject timer;
     public bool paused;
+    public GameObject timer;
+    public GameObject pauseScreen;
+    private bool pauseScreenActive;
+    public GameObject controlScreen;
+
 
     public Text[] buttons;
     public int currentSelect;
     public Color selected;
     public Color deselected;
+    public GameObject pointer;
+    private Vector3 pointerpos;
 
     public string player1input;
-    public string player2input;
     private bool cantMove = false;
 
-    void Start()
+    private void Start()
     {
-        
     }
 
-   
     void Update()
     {
         Pause();
@@ -35,24 +39,37 @@ public class PauseScreen : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Joystick1Button9))
         {
-            if (pauseScreen.activeSelf==false)
+            if (pauseScreen.activeSelf == false)
             {
                 pauseScreen.SetActive(true);
                 timer.SetActive(false);
                 paused = true;
+                pauseScreenActive = true;
 
                 Time.timeScale = 0;
                 currentSelect = 0;
+                pointerpos = pointer.transform.position;
             }
             else
             {
+                unpause();
+            }
+        }
+    }
+
+    void unpause()
+    {
+            if (pauseScreen.activeSelf == true)
+            {
+                pointer.transform.position = pointerpos;
                 pauseScreen.SetActive(false);
+                controlScreen.SetActive(false);
                 timer.SetActive(true);
                 paused = false;
+                pauseScreenActive = false;
 
                 Time.timeScale = 1;
             }
-        }
     }
 
     void selectColor()
@@ -61,7 +78,7 @@ public class PauseScreen : MonoBehaviour
         {
             for (int i = 0; i < buttons.Length; i++)
             {
-                if (i==currentSelect)
+                if (i == currentSelect)
                 {
                     buttons[i].color = selected;
                 }
@@ -75,29 +92,65 @@ public class PauseScreen : MonoBehaviour
 
     void selection()
     {
-       
-        if ((Input.GetAxis(player1input)>GameManager.instance.deadZone) && !cantMove)
+        if (pauseScreenActive)
         {
-            cantMove = true;
-            
-        }else if ((Input.GetAxis(player1input) < -GameManager.instance.deadZone) && !cantMove)
-        {
-            cantMove = true;
-            Debug.Log("movedown");
-        }else if ((Input.GetAxis(player1input) < GameManager.instance.deadZone) &&(Input.GetAxis(player1input) > -GameManager.instance.deadZone))
-        {
-            cantMove = false;
-            Debug.Log("canMove");
+            if ((Input.GetAxis(player1input) > GameManager.instance.deadZone) && !cantMove)
+            {
+                if (currentSelect > 0)
+                {
+                    cantMove = true;
+                    currentSelect--;
+                    pointer.transform.position += new Vector3(0, 32, 0);
+                }
+            }
+            else if ((Input.GetAxis(player1input) < -GameManager.instance.deadZone) && !cantMove)
+            {
+                if (currentSelect < buttons.Length - 1)
+                {
+                    cantMove = true;
+                    currentSelect++;
+                    pointer.transform.position += new Vector3(0, -32, 0);
+                }
+            }
+            else if ((Input.GetAxis(player1input) < GameManager.instance.deadZone) &&
+                     (Input.GetAxis(player1input) > -GameManager.instance.deadZone))
+            {
+                cantMove = false;
+                Debug.Log("canMove");
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Joystick1Button2))
         {
-            if (currentSelect==0)
+            if (pauseScreenActive)
             {
-                pauseScreen.SetActive(false);
-                               timer.SetActive(true);
-                               paused = false;
-                               Time.timeScale = 1; 
+                if (currentSelect == 0)
+                {
+                    unpause();
+                }
+                else if (currentSelect == 1)
+                {
+                    pauseScreen.SetActive(false);
+                    controlScreen.SetActive(true);
+                    pauseScreenActive = false;
+                }
+                else if (currentSelect == 2)
+                {
+                    SceneManager.LoadScene(0);
+                }
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+        {
+            if (pauseScreenActive)
+            {
+                unpause();
+            }
+            else if ((!pauseScreenActive) && (paused))
+            {
+                pauseScreen.SetActive(true);
+                controlScreen.SetActive(false);
+                pauseScreenActive = true;
             }
         }
     }
