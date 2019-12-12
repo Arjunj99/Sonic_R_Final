4c37d97c19ff998aae4713d8e85c92512eaf60a2
sfarn <sfarn48@gmail.com>
2019-12-12 14:00:42 -0500

    Formatted results page, added in replay button

---
+++ Sonic R Final/Assets/Replay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Replay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button2))
+        {
+            SceneManager.LoadScene(0);
+            GameManager.instance.p1totalTime = 0;
+            GameManager.instance.p1roundOneTime = 0;
+            GameManager.instance.p1roundTwoTime = 0;
+            GameManager.instance.p1roundThreeTime = 0;
+            GameManager.instance.p1roundFourTime = 0;
+            GameManager.instance.p1roundFiveTime = 0;
+            GameManager.instance.p2totalTime = 0;
+            GameManager.instance.p2roundOneTime =0;
+            GameManager.instance.p2roundTwoTime = 0;
+            GameManager.instance.p2roundThreeTime = 0;
+            GameManager.instance.p2roundFourTime = 0;
+            GameManager.instance.p2roundFiveTime = 0;
+        }
+        
+    }
+}
